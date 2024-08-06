950357568c2b8a3bb585a60bbd6cfdefcf15c852
Filip Jamroga <37691851+Jonson26@users.noreply.github.com>
2024-08-06 13:56:54 +0200

    Removed old unused variants of the KBCexpansion function. In particular the removed functions were:  * KBCexpansionNoEpsilon  * KBCexpansionMundaneEpsilon

--- src/KnowledgeBasedConstruction.hpp
+++ src/KnowledgeBasedConstruction.hpp
@@ -20,7 +20,5 @@ using namespace std;
 //void mockKBC(GlobalModel *const gm);
 //void mockLocalKBC(Agent *const a);
 void KBCprojection(GlobalModel *const gm, int agent_id);
-Agent* KBCexpansionNoEpsilon(GlobalModel *const gm, int agent_id);
-Agent* KBCexpansionMundaneEpsilon(GlobalModel *const gm, int agent_id);
 Agent* KBCexpansion(GlobalModel *const gm, int agent_id);
 GlobalModel* cloneGlobalModel(LocalModels* localModels, Formula* formula);
